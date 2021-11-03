8d5e6836201a7e367e0f8adfe47be75f5521fb80
Jordan Fleming <jordano.fleming@gmail.com>
2021-11-02 18:22:31 -0700

    commented out header/footer to help deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
-import Header from './components/Header.js';
+// import Header from './components/Header.js';
 import Main from './components/Main.js';
-import Footer from './components/Footer.js';
+// import Footer from './components/Footer.js';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
